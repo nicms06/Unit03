9b9e9bc0224c6ac582e57bf27eeca7a154c8093a
nicms06 <nicolas.mingorance@iesnervion.es.com>
2025-12-01 14:10:18 +0100

    Ejercicio 7 empezado

---
+++ src/Arrays/Exercise07.java
@@ -0,0 +1,25 @@
+/*
+ * Realiza un programa que pida la temperatura media que ha hecho en cada mes de un determinado año y 
+ * las almacene en una tabla (una posición por mes). A continuación, se debe mostrar un diagrama de barras 
+ * horizontales con esos datos. Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro carácter.
+ */
+
+package Arrays;
+
+import java.util.Scanner;
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+
+		//Creamos un nuevo Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		//Cerramos el Scanner
+		sc.close();
+		
+	}
+
+}
