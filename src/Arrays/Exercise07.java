0670ff10778092f78c487fa7dd2c4f147e532adc
nicms06 <nicolas.mingorance@iesnervion.es.com>
2025-12-01 14:38:30 +0100

    Ejercicio 7 resuelto

--- src/Arrays/Exercise07.java
+++ src/Arrays/Exercise07.java
@@ -15,7 +15,25 @@ public class Exercise07 {
 		//Creamos un nuevo Scanner
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaramos las variables
+		double tabla[] = new double[12];
 		
+		//Pedimos los datos
+		for (int i = 0; i < tabla.length; i++) {
+			System.out.println("Temperatura del mes " + (i+1) + ": ");
+			tabla[i] = sc.nextInt();
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < tabla.length; i++) {
+			System.out.println("Temperatura del mes " + (i+1) + ": ");
+			
+			for (int j = 0; j < tabla[i]; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 		//Cerramos el Scanner
 		sc.close();
