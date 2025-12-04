a7bb06cecaf19fee7aa52583c026bc18ed4eb45b
nicms06 <nicolas.mingorance@iesnervion.es.com>
2025-12-04 13:29:53 +0100

    Ejercicios 7 y 8 resueltos

--- src/Arrays/Exercise07.java
+++ src/Arrays/Exercise07.java
@@ -1,43 +1,35 @@
-/*
- * Realiza un programa que pida la temperatura media que ha hecho en cada mes de un determinado año y 
- * las almacene en una tabla (una posición por mes). A continuación, se debe mostrar un diagrama de barras 
- * horizontales con esos datos. Las barras del diagrama se pueden dibujar a base de asteriscos o cualquier otro carácter.
- */
-
 package Arrays;
 
 import java.util.Scanner;
 
 public class Exercise07 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//Creamos un nuevo Scanner
-		Scanner sc = new Scanner(System.in);
-		
-		//Declaramos las variables
-		double tabla[] = new double[12];
-		
-		//Pedimos los datos
-		for (int i = 0; i < tabla.length; i++) {
-			System.out.println("Temperatura del mes " + (i+1) + ": ");
-			tabla[i] = sc.nextInt();
-		}
-		
-		System.out.println();
-		
-		for (int i = 0; i < tabla.length; i++) {
-			System.out.println("Temperatura del mes " + (i+1) + ": ");
-			
-			for (int j = 0; j < tabla[i]; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		//Cerramos el Scanner
-		sc.close();
-		
-	}
+        Scanner sc = new Scanner(System.in);
 
+        double tabla[] = new double[12];
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                          "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+        // Pedimos temperaturas
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.print("Temperatura media de " + meses[i] + ": ");
+            tabla[i] = sc.nextDouble();
+        }
+
+        System.out.println("\nDiagrama de temperaturas:");
+        
+        // Dibujamos barras
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.printf("%-12s: ", meses[i]);
+            int cantidadAsteriscos = (int) Math.round(tabla[i]); // Convertimos a int para dibujar
+            for (int j = 0; j < cantidadAsteriscos; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
---
+++ src/Arrays/Exercise08.java
@@ -0,0 +1,59 @@
+/*
+ * Crea un programa que cree un array de enteros de tamaño 100 y lo rellene con valores enteros aleatorios entre 1 y 10. 
+ * Luego pedirá un valor N y mostrará en qué posiciones del array aparece N. 
+ */
+
+package Arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise08 {
+
+	public static void main(String[] args) {
+
+		//Create a new Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		Random rand = new Random();
+		
+		//Declaramos el array de 100 números
+		int array[] = new int [100];
+		
+		//Declaramos la variable num y el booleano encontrado
+		int num;
+		boolean encontrado = false;
+		
+		//Rellenamos el array
+		for (int i = 0; i <array.length; i++) {
+			array[i] = rand.nextInt(1, 11);
+		}
+		
+		//Pedimos un valor num
+		System.out.print("Introduce un valor entre 1 y 10: ");
+		num = sc.nextInt();
+		
+		while (num < 1 ||num > 10) {
+			System.out.print("Error, inténtelo de nuevo: ");
+			num = sc.nextInt();
+		}
+		
+		//Ahora vemos las posiciones del número
+		for (int i = 0; i <array.length; i++) {
+			if (array[i] == num) {
+				encontrado = true;
+				System.out.println("Encontrado en: " + i);
+			}
+		}
+		
+		//Si el boleano buscar es falso, , no se ha encontrado el número
+		if(!encontrado) {
+			System.out.println("Elemento no encontrado");
+		}
+		
+		//Cerramos el Scanner
+		sc.close();
+		
+	}
+
+}
