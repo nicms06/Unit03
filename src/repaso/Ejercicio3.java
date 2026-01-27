61e76f071befcdc88eedcfc32628827048535e4c
Your NamenicmsHUB <you@example.comnicms2006rp@gmail.com>
2026-01-27 16:16:20 +0100

    Ejercicio 3 tipo examen terminado

--- src/repaso/Ejercicio3.java
+++ src/repaso/Ejercicio3.java
@@ -12,31 +12,82 @@ public class Ejercicio3 {
 		
 		//Declaración de un array con 5 palabras clave
 		String[] palabrasClave = {"exploración", "olas", "aventura", "misterio", "playa"};
-		
-		
+
+        //Creación de un array donde llamamos a la función palabrasEncontradas para guardar los valores de las palabras que fueron encontradas
+        String [] encontradas = palabrasEncontradas(texto, palabrasClave);
+
+        //Llamamos a la función mostrarResultado
+        System.out.println("Las palabras encontradas fueron: ");
+        mostrarResultado(encontradas);
 	}
-	
-	//Función que devuelve True si el texto contiene la palabra clave
-	static boolean contienePalabra(String texto, String palabra) {
-		String textoSeparado[] = texto.split(" "); //Creación de un string que separa el texto por palabras
-		for (String palabraTexto: textoSeparado) { //Recorremos el texto
-			if (palabraTexto.equalsIgnoreCase(palabra)) { //Si encontramos la palabra devolvemos true
-				return true;
-			}
-		}
-		return false; //Si no hemos encontrado la palabra devolvemos false
-	}
-	
-	//Función que devuelve un nuevo array con solo las palabras clave que aparecen en el texto.
-	static String[] palabrasEncontradas(String texto, String[] claves) {
-		int longitud = 0;
-		for (String palabra : claves) { //recorremos el texto 
-			if (contienePalabra(texto, palabra)) { //
-				longitud ++;
-			}
-		}
-		
-		
-	}
+
+    /**
+     * Función que devuelve True si el texto contiene la palabra clave
+     * @param texto texto completo
+     * @param palabra palabra que queremos comprobar si está dentro del texto
+     * @return devolvemos true o false dependiendo de si coincude alguna plabra o no
+     */
+    static boolean contienePalabra(String texto, String palabra){
+        String []textoSeparado = texto.split(" "); //Separamos cada una de las palabas del texto y las guardamos en un array
+
+        for(String palabrasSeparadas: textoSeparado){ //Recorremos el array creado
+            if (palabra.trim().equalsIgnoreCase(palabrasSeparadas)){ //Si alguna palabra coincide con alguna de las plabras claves devolvemos true
+                return true;
+            }
+        }
+        return false; //Si ninguna palabra coincide, devolvemos false
+    }
+
+    /**
+     * Función que devuelve un nuevo array con solo las palabras clave que aparecen en el texto.
+     * @param texto texto completo
+     * @param claves array con las palabras clabes
+     * @return devolvemos las palabras que aparecen en el texto
+     */
+    static String[] palabrasEncontradas(String texto, String[] claves){
+        int longitud = 0;
+
+        for (String c : claves){
+            //Llamamos a la función contienePalabra
+            if (contienePalabra(texto, c)){ //Si contiene la palabra sumamos 1 al contador de longitud
+                longitud ++;
+            }
+        }
+
+        //Creamos el array del tamaño de las palabras que aparecen
+        String[] palabrasEnTexto = new String[longitud];
+
+        //Añadimos las palabras
+        int indice = 0;
+        for (String c : claves){
+            //Llamamos a la función contienePalabra
+            if (contienePalabra(texto, c)){ //Si contiene la palabra sumamos 1 al contador de longitud
+                palabrasEnTexto[indice] = c;
+                indice++;
+            }
+        }
+
+        return palabrasEnTexto; //Devolvemos las palabras que aparecen en el texto
+    }
+
+    /**
+     * Función que muestra el resultado del ejercicio
+     * @param encontradas array que contiene las palabras encontradas
+     */
+    static void mostrarResultado(String[] encontradas){
+
+        if(encontradas.length == 0){ //Si el array tiene longitud 0 mostramos un mensaje de que no se ha encontrado ninguna palabra
+            System.out.println("No se ha encontrado ninguna palabra.");
+        } else { //Si la longitud es mayor a 0 mostramos
+            for (int i = 0; i < encontradas.length; i++){ //recorremos el array
+                System.out.print(encontradas[i]); //Mostramos la posición actial del bucle
+
+                if (i < encontradas.length -1){ //Mientras no nos encontremos en la útlima interacción del bucle vamos a imprimir una coma
+                    System.out.print(", ");
+                }
+            }
+        }
+    }
+
 
 }
